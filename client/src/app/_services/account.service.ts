84713705bd04afcf3ae9ebc098c5a1a97f626875
Lukas Brinkmann <lukas.brinkmann@adesso.de>
2024-09-27 11:03:23 +0200

    End of Section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { tap } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = 'https://localhost:5001/api/';
+  currentUser = signal<User | null>(null);
+
+  constructor() { }
+
+  login(model: any) {
+    return this.http.post<User>(`${this.baseUrl}account/login`, model).pipe(
+      tap(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  register(model: any) {
+    return this.http.post<User>(`${this.baseUrl}account/register`, model).pipe(
+      tap(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
